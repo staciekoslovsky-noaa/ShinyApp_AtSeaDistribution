6da5f0f0ed42051eb6ce37e61db6ddc6c494d7f2
Christine Kwon <christinekwon@MacBook-Air.local>
2024-07-18 13:37:02 -0500

    readded local file helper for now

--- ASDShiny/helper_functions.R
+++ ASDShiny/helper_functions.R
@@ -96,4 +96,38 @@ data source is attempting to “sample” the underlying species distribution. T
 existing data (i.e., no new data collection is anticipated, only processing and analysis of existing data).', style = 'color: #005b96')
   
 
+load_all_filest <- function(directory) {
+  # Loads all files from data folder 
+  
+  # Inputs: directory[str]: pathway to files
+  
+  # Returns: None, sets file name to loaded data
+  
+  
+  # List all .RData files in the directory
+  files <- list.files(directory, pattern = "\\.RData$", full.names = TRUE)
+  #print(files) debugging
+  if (length(files) == 0) {
+    stop("No .RData files found in the directory.")
+  }
+  
+  for (file in files) {
+    # Get base name of the file (without directory and extension)
+    file_base_name <- tools::file_path_sans_ext(basename(file))
+    
+    temp_env <- new.env()
+    load(file, envir = temp_env)
+    
+    # Assuming the file contains only one object, get its name and data
+    # Explore further this globalenv/tempenv issue 
+    object_name <- ls(temp_env)[1]
+    data <- get(object_name, envir = temp_env)
+    
+    # Assign the data to a variable named after the file
+    assign(file_base_name, data, envir = .GlobalEnv)
+    #print(file_base_name)
+  }
+  
+  print("Finished loading.")
+}
   
