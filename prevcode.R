14a5f2afd56f0ffc99a8e0f419e5982cf014ccfa
Christine Kwon <christinekwon@MacBook-Air.local>
2024-07-18 11:36:09 -0500

    local access func

--- prevcode.R
+++ prevcode.R
@@ -72,3 +72,42 @@ output$getshape <- downloadHandler(
   },
   content = function(file){
     st_write(drawn_polygon, 'data/shapefile.shp')})
+
+
+
+# DOES NOT INCLUDE POPhexagons_sf file!!!
+# Previous local data download function
+load_all_filest <- function(directory) {
+  # Loads all files from data folder 
+  
+  # Inputs: directory[str]: pathway to files
+  
+  # Returns: None, sets file name to loaded data
+  
+  
+  # List all .RData files in the directory
+  files <- list.files(directory, pattern = "\\.RData$", full.names = TRUE)
+  #print(files) debugging
+  if (length(files) == 0) {
+    stop("No .RData files found in the directory.")
+  }
+  
+  for (file in files) {
+    # Get base name of the file (without directory and extension)
+    file_base_name <- tools::file_path_sans_ext(basename(file))
+    
+    temp_env <- new.env()
+    load(file, envir = temp_env)
+    
+    # Assuming the file contains only one object, get its name and data
+    # Explore further this globalenv/tempenv issue 
+    object_name <- ls(temp_env)[1]
+    data <- get(object_name, envir = temp_env)
+    
+    # Assign the data to a variable named after the file
+    assign(file_base_name, data, envir = .GlobalEnv)
+    #print(file_base_name)
+  }
+  
+  print("Finished loading.")
+}
